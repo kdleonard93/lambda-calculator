2f6c0aef0741477647c5eacb80d0425b3f2aa98d
Kyle Leonard <kdleonard@icloud.com>
2019-08-25 18:43:20 -0500

    Fixed branch issue

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,11 +1,14 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { numbers } from "./../../../data";
+import NumberButton from "./NumbersButton";
 //import any components needed
 
 //Import your array data to from the provided data file
 
-const Numbers = () => {
+const Numbers = props => {
   // STEP 2 - add the imported data to state
+
+  const [num] = useState(numbers);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
