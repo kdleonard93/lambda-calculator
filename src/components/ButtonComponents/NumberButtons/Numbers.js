f4ef1856f743dc593a2a65b9ab39b08b230d120e
Kyle Leonard <kdleonard@icloud.com>
2019-08-26 16:57:25 -0500

    Completed Numbers.js
    Understood concepts after going over react docs again

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -9,13 +9,22 @@ const Numbers = (props) => {
   // STEP 2 - add the imported data to state
 
   const [numberState, setNumberState] = useState(numbers);
+
+console.log(numberState);
+
   return (
     <div>
-
-      
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+
+    {
+      numberState.map(num => {
+        return <NumberButton key={number} text ={number} addNumber = {props.addNumber} />;
+      })
+    }
     </div>
   );
 };
+
+export default Numbers;
